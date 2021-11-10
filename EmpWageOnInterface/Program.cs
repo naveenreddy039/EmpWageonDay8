0e6e80d61490435537c9f88bc9b8afa82237ed8c
naveen <naveenreddy.k07@gmail.com>
2021-11-10 12:16:37 +0530

    second commit

---
+++ EmpWageOnInterface/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EmpWageOnInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int IsFullTime = 1;
+                Random random = new Random();
+                int empCheck = random.Next(0, 2);
+                if(empCheck==IsFullTime)
+                {
+                    Console.WriteLine("Employee is Present");
+                }
+                else
+                {
+                    Console.WriteLine("Employee is absent");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+        }
+    }
+}
