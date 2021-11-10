a0b275877eca298cbbfe0421af65487a02b58965
naveen <naveenreddy.k07@gmail.com>
2021-11-10 16:19:48 +0530

    third commit

---
+++ UC4Emp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace UC4Emp
+{
+    public class Program
+    {
+        public const int IsFullTime = 1;
+        public const int IsPartTime = 2;
+        public const int EmpRatePerHr = 20;
+        public static void Main(string[] args)
+        {
+            
+            try
+            {
+                int empHrs = 0;
+                int EmpWage = 0;
+                Random random = new Random();
+                int empCheck = random.Next(1, 2);
+                switch(empCheck)
+                {
+                    case IsPartTime:
+                        empHrs = 8;
+                        break;
+                    case IsFullTime:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                EmpWage = empHrs * EmpRatePerHr;
+                Console.WriteLine("employe wage" + EmpWage);
+            }
+            catch
+            {
+                Console.WriteLine("Employee is Parttime");
+            }
+        }
+    }
+}
