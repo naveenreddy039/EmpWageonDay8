6b2a235298de8d49c646e12da42d0df498d544cb
naveen <naveenreddy.k07@gmail.com>
2021-11-10 13:49:10 +0530

    second commit

---
+++ UC3Emp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace UC3Emp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int IsFullTime = 1;
+                int IsPartTime = 2;   
+                int EmpRatePerHr = 20;
+                int empHrs = 0;
+                int EmpWage = 0;
+                Random random = new Random();
+                int empCheck = random.Next(1, 2);
+                if (empCheck == IsFullTime)
+                {
+                    empHrs = 4;
+                }
+                else if(empCheck==IsPartTime)
+                {
+                    empHrs = 8;
+                }
+                else
+                {
+                    empHrs = 0;
+                }
+                EmpWage = empHrs * EmpRatePerHr;
+                Console.WriteLine(EmpWage);
+            }
+            catch
+            {
+                Console.WriteLine("part Time");
+            }
+        }
+    }
+}
