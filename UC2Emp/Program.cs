d50c48258f86cd51b43570a0bf62272631df896d
naveen <naveenreddy.k07@gmail.com>
2021-11-10 12:12:13 +0530

    firt commit

---
+++ UC2Emp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UC2Emp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int IsFullTime = 1;
+                int EmpRatePerHr = 20;
+                int empHrs = 0;
+                int EmpWage = 0;
+                Random random = new Random();
+                int empCheck = random.Next(1, 2);
+                if (empCheck == IsFullTime)
+                {
+                    empHrs = 8;
+                }
+                else
+                {
+                    empHrs = 0;
+                }
+                EmpWage = empHrs * EmpRatePerHr;
+                Console.WriteLine(EmpWage);
+            }
+            catch
+            {
+                Console.WriteLine("Absent");
+            }
+        }
+    }
+}
